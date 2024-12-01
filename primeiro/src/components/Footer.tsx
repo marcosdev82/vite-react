be8f44cf393b82736c21da69385422a34063e0c9
mikek182 <marcosvinicius@geekwork.com.br>
2024-12-01 11:37:02 -0300

    Correação da footer

--- primeiro/src/components/Footer.tsx
+++ primeiro/src/components/Footer.tsx
@@ -1,8 +1,9 @@
-import "./Footere.css";
+import "./Footer.css";
 
 export function Footer() {
     return (
         <footer>
+             <hr />
             <p>Todos os direitos reservados</p>
         </footer>
     )
